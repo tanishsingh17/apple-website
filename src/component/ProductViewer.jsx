b0cf374ae6e78a0d9e19afcc8a46702937943671
Tanish Singh Thakur <tanish.singhthakur123@gmail.com>
2025-11-07 12:03:36 +0530

    correction

--- src/component/ProductViewer.jsx
+++ src/component/ProductViewer.jsx
@@ -5,7 +5,7 @@ import { Box , OrbitControls } from '@react-three/drei';
 import MacbookModel14 from './models/Macbook-14';
 import { useMediaQuery } from 'react-responsive';
 import { Suspense } from 'react';
-import StudioLights from './StudioLights';
+import StudioLights from './three/StudioLights';
 import ModelSwitcher from './three/ModelSwitcher';
 
 
